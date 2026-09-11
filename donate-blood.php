c6c7a0e44137a5701f92c29ab9a68c27fd8b868d
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-09-11 17:08:28 +0530

    Update donate-blood.php

--- donate-blood.php
+++ donate-blood.php
@@ -30,7 +30,7 @@ class script{
         public function __construct(){
 
                 $server = "mysqldb";
-                $user = "root";
+                $user = "admin";
                 $pass = "admin123";
                 $db = "customers";
 
